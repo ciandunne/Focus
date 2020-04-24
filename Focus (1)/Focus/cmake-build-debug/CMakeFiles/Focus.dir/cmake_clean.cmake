98a0dffb6130ff282e31eba426330bc1a80ef810
cianopiano <ciandunne2@gmail.com>
2020-04-24 20:29:09 +0100

    intialising the game

---
+++ Focus (1)/Focus/cmake-build-debug/CMakeFiles/Focus.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Focus.dir/game_init.c.obj"
+  "CMakeFiles/Focus.dir/input_output.c.obj"
+  "CMakeFiles/Focus.dir/main.c.obj"
+  "Focus.exe"
+  "Focus.exe.manifest"
+  "Focus.pdb"
+  "libFocus.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Focus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
